3a94f2fa95d5987b29b4b07a140dfce7ee7d75fd
Asari Mitsuteru <asari.mtr@gmail.com>
2012-07-19 17:23:37 +0900

    .lessも入れちゃう

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .zshrc.mine .vimrc .lesskey .tmux.conf .tigrc .irbrc .ctags .gitconfig .curlrc)
+DOT_FILES=( .zshrc .zshrc.mine .vimrc .lesskey .less .tmux.conf .tigrc .irbrc .ctags .gitconfig .curlrc)
 
 for file in ${DOT_FILES[@]}
 do
