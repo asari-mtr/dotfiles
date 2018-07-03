2ceb305503c92a585e4d96fecaf2bbafbffa07ce
Asari Mitsuteru <mitsuteru.asari@supership.jp>
2018-07-03 10:30:06 +0900

    Add .pyenv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zsh .zshrc .zshrc.mine .vimrc .lesskey .less .tmux.conf .tigrc .pythonrc .irbrc .ctags .gitconfig .gitignore .curlrc)
+DOT_FILES=( .zsh .zshrc .zshrc.mine .vimrc .lesskey .less .tmux.conf .tigrc .pythonrc .irbrc .ctags .gitconfig .gitignore .curlrc .pyenv)
 
 for file in ${DOT_FILES[@]}
 do
